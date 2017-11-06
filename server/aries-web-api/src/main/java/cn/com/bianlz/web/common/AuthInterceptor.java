6b7d07affedd5ff5464f9acd883290331e30bc72
bianlanzhou <bianlanzhou@gmail.com>
2017-11-06 10:51:40 +0800

    add logger

--- server/aries-web-api/src/main/java/cn/com/bianlz/web/common/AuthInterceptor.java
+++ server/aries-web-api/src/main/java/cn/com/bianlz/web/common/AuthInterceptor.java
@@ -6,6 +6,8 @@ import cn.com.bianlz.user.api.user.User;
 import cn.com.bianlz.web.App;
 import cn.com.bianlz.web.HttpCodeUtils;
 import cn.com.bianlz.web.client.UserServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -21,26 +23,32 @@ import java.io.IOException;
  * Description
  */
 public class AuthInterceptor extends HandlerInterceptorAdapter {
-
+    private static final Logger logger = LoggerFactory.getLogger(AuthInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         UserServiceClient userServiceClient = App.getBean(UserServiceClient.class,"cn.com.bianlz.web.client.UserServiceClient");
         if (handler instanceof HandlerMethod){
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            Authorizition auth = handlerMethod.getBeanType().getAnnotation(Authorizition.class);
-            User user = null;
-            String token = request.getHeader("token");
-            if(token!=null){
-                Result<User> result = userServiceClient.getUserByToken(token);
-                user = result.getData();
-            }
-            if(auth!=null&&auth.auth()){
-                if(user==null){
-                    HttpCodeUtils.set403(response);
-                    return false;
+            try {
+                HandlerMethod handlerMethod = (HandlerMethod) handler;
+                Authorizition auth = handlerMethod.getBeanType().getAnnotation(Authorizition.class);
+                User user = null;
+                String token = request.getHeader("token");
+                if(token!=null){
+                    Result<User> result = userServiceClient.getUserByToken(token);
+                    user = result.getData();
                 }
+                if(auth!=null&&auth.auth()){
+                    if(user==null){
+                        HttpCodeUtils.set403(response);
+                        return false;
+                    }
+                }
+                request.getSession().setAttribute("user",user);
+            }catch (Exception ex){
+                ex.printStackTrace();
+                logger.debug("auth intercepter exception:"+ex.getMessage());
             }
-            request.getSession().setAttribute("user",user);
+
         }
         return super.preHandle(request, response, handler);
     }
