0ce116b5aefc1e231ce91fe047731a776bbf8a5f
bianlanzhou <bianlanzhou@gmail.com>
2017-11-09 18:25:58 +0800

    去掉注释

--- server/aries-schedule/src/main/java/cn/com/bianlz/cron/DataCron.java
+++ server/aries-schedule/src/main/java/cn/com/bianlz/cron/DataCron.java
@@ -24,14 +24,14 @@ public class DataCron {
     private ConsumeStatisticsTask consumeStatisticsTask;
     @Autowired
     private ContextTask contextTask;
-    @Scheduled(cron = "0 */1 * * * * ")
+    @Scheduled(cron = "0 */30 * * * * ")
     public void schedule(){
-       // scheduleTask.run();
+        scheduleTask.run();
         contextTask.run();
     }
     @Scheduled(cron = "0 */10 * * * * ")
     public void consume(){
-       // consumeStatisticsTask.run();
+        consumeStatisticsTask.run();
     }
 
 }
